ac9320de5734b4c0cd0e8c793daee96ef89c00ae
Kirill Trezubov <kirill3zoff@gmail.com>
2024-04-05 17:13:57 +0500

    OA-upd: Finish 48

--- Source/ObstacleAssault/MovingPlatform.cpp
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -11,10 +11,8 @@ void AMovingPlatform::BeginPlay()
 {
     Super::BeginPlay();
 
-    MyInt = 9;
-
-    APlusB = InputA + InputB;
-    APlusBFloat = InputFloatA + InputFloatB;
+    MyVector.Y = MyX;
+    
 }
 
 void AMovingPlatform::Tick(float DeltaTime)
